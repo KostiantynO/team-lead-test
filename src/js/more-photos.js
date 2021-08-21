4ecce8b4c9b931d9c3386191dd97885ffceb6c3b
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-08-22 01:12:31 +0300

    js

--- src/js/more-photos.js
+++ src/js/more-photos.js
@@ -1,68 +1,37 @@
 'use strict';
 (() => {
-  const mission = `Mission:
-    1. Синхронизировать клик по картинке между мелким фото и оригиналом в модалке.
-    2. Снять active-slide со всех и назначить только одной (или трем) выбранным фото.
-    3. Или чтобы убирался src с фотки, которую нужно отобразить. (easy method)`;
-  const btn = document.querySelector('[data-photos-list]');
-  const overlay = document.querySelector('[data-gallery-modal]');
-  const modalImgSrc = document.querySelector('[data-gallery-image]');
-  const body = document.querySelector('body');
-  const btnBack = document.querySelector('[data-gallery-back]');
-  const btnNext = document.querySelector('[data-gallery-next]');
+  const previewList = document.querySelector('[data-preview-list]');
+  const photosLoaderBtn = document.querySelector('[data-photos-loader]');
+  const liBelowFold = document.querySelectorAll('[data-li-below-fold]');
+  const photosBelowFold = document.querySelectorAll('[data-photos-below-fold]');
+  const liBelowFoldLength = liBelowFold.length;
 
-  alert(`${refs.mission}`);
-  let previewListImgs = [];
-  previewListImgs.push(...btn.childNodes);
+  let photosHaveSrc = 0;
+  const loadPhotos = () => {
+    for (let i = 0; i < 2; ++i) {
+      liBelowFold[i].classList.remove('is-hidden');
 
-  const previewListMap = previewListImgs.filter(num => {
-    return num.nodeName === 'LI';
-  });
-  const listImgPicture = previewListMap.map(num => {
-    console.log('Hello');
-    return num.children[0].children[0].children[0].attributes[0].nodeValue;
-  });
-  let counter = 0;
+      console.log(liBelowFold[i].childNodes[1].childNodes[1]);
 
-  /*
-const modalOpen = (even) => {
-    even.preventDefault()
-    if (even.target.nodeName !== "IMG") {
-        return
+      photosBelowFold[i].setAttribute('src', '#');
+
+      if (photosBelowFold[i].hasAttribute('src') === true) {
+        photosBelowFold[i].removeAttribute('data-photos-below-fold');
+        if (photosHaveSrc < 8) {
+          ++photosHaveSrc;
+        }
+        console.log(photosHaveSrc);
+      }
     }
-    counter = Number(even.target.parentElement.attributes.id.value);
-    btnNext.addEventListener('click', nextImg);
-    btnBack.addEventListener('click', backImg)
-    bodyEl.setAttribute("style", "overflow: hidden");
-    modalImgSrc.setAttribute('src', listImgPicture[counter]);
-    backdrop.classList.add('is-active');
-    backdrop.addEventListener('click', modalClose)
-}
-const nextImg = () => {
-    if (counter === 11) {
-        return
+
+    if (photosHaveSrc === previewList.childElementCount) {
+      photosHaveSrc = true;
     }
-    counter = counter + 1;
-    console.log(counter);
-    modalImgSrc.setAttribute('src', listImgPicture[counter]);
-}
-const backImg = () => {
-    if (counter === 0) {
-        return
+    const areAllPhotosLoaded = photosHaveSrc || false;
+    if (areAllPhotosLoaded === true) {
+      photosLoaderBtn.removeEventListener('click', loadPhotos());
     }
-    counter = counter - 1
-    console.log(counter)
-    modalImgSrc.setAttribute('src', listImgPicture[counter]);
-}
-const modalClose = (even) => {
-    even.preventDefault()
-    if (even.target.className !== "backdrop is-active") {
-        return
-    }
-    bodyEl.setAttribute("style", "");
-     modalImgSrc.setAttribute('src', '');
-    backdrop.removeEventListener('click', modalClose)
-    backdrop.classList.remove('is-active');
-}
-btn.addEventListener('click', modalOpen); */
+  };
+
+  photosLoaderBtn.addEventListener('click', loadPhotos);
 })();
---
+++ src/js/rus.js
@@ -0,0 +1,54 @@
+const btn = document.querySelector('.masonry');
+const imgPic = document.querySelectorAll('.masonry__img-task');
+const backdrop = document.querySelector('.backdrop');
+const imgSrc = document.querySelector('.modal-window__img');
+const bodyEl = document.querySelector('body');
+const btnBack = document.querySelector('.modal-window__back');
+const btnNext = document.querySelector('.modal-window__next');
+let listImg = [];
+
+listImg.push(...imgPic);
+
+const listImgPicture = listImg.map(({ children }) => {
+  return children[0].src;
+});
+let counter = 0;
+
+const modalOpen = even => {
+  even.preventDefault();
+  if (even.target.nodeName !== 'IMG') {
+    return;
+  }
+  counter = Number(even.target.parentElement.attributes.id.value);
+  btnNext.addEventListener('click', nextImg);
+  btnBack.addEventListener('click', backImg);
+  bodyEl.setAttribute('style', 'overflow: hidden');
+  imgSrc.setAttribute('src', listImgPicture[counter]);
+  backdrop.classList.add('is-active');
+  backdrop.addEventListener('click', modalClose);
+};
+const nextImg = () => {
+  if (counter === 11) {
+    return;
+  }
+  counter = counter + 1;
+  imgSrc.setAttribute('src', listImgPicture[counter]);
+};
+const backImg = () => {
+  if (counter === 0) {
+    return;
+  }
+  counter = counter - 1;
+  imgSrc.setAttribute('src', listImgPicture[counter]);
+};
+const modalClose = even => {
+  even.preventDefault();
+  if (even.target.className !== 'backdrop is-active') {
+    return;
+  }
+  bodyEl.setAttribute('style', '');
+  imgSrc.setAttribute('src', '');
+  backdrop.removeEventListener('click', modalClose);
+  backdrop.classList.remove('is-active');
+};
+btn.addEventListener('click', modalOpen);
