119f235f033464cc0f1a8fbf3ac7e45234c6d583
Kostiantyn Ochenash <Ochenashk@gmail.com>
2021-08-22 21:44:03 +0300

    Adds functionality for photosLoaderBtn to display 3 new LI's on click in mobile view

--- src/js/more-photos.js
+++ src/js/more-photos.js
@@ -4,33 +4,19 @@
   const photosLoaderBtn = document.querySelector('[data-photos-loader]');
   const liBelowFold = document.querySelectorAll('[data-li-below-fold]');
   const photosBelowFold = document.querySelectorAll('[data-photos-below-fold]');
-  const liBelowFoldLength = liBelowFold.length;
+  const liBelowFoldCount = liBelowFold.length - 1;
 
   let photosHaveSrc = 0;
   const loadPhotos = () => {
-    for (let i = 0; i < 2; ++i) {
-      liBelowFold[i].classList.remove('is-hidden');
-
-      console.log(liBelowFold[i].childNodes[1].childNodes[1]);
-
-      photosBelowFold[i].setAttribute('src', '#');
-
-      if (photosBelowFold[i].hasAttribute('src') === true) {
-        photosBelowFold[i].removeAttribute('data-photos-below-fold');
-        if (photosHaveSrc < 8) {
-          ++photosHaveSrc;
-        }
-        console.log(photosHaveSrc);
-      }
+    for (let i = 0; i <= 2; ++i) {
+      liBelowFold[photosHaveSrc].classList.remove('is-hidden');
+      photosBelowFold[photosHaveSrc].setAttribute('src', '#');
+      // console.log(`Attribute 'src' has been set ${photosHaveSrc + 1} times`);
+      photosBelowFold[photosHaveSrc].removeAttribute('data-photos-below-fold');
+      if (photosHaveSrc < liBelowFoldCount) ++photosHaveSrc;
     }
 
-    if (photosHaveSrc === previewList.childElementCount) {
-      photosHaveSrc = true;
-    }
-    const areAllPhotosLoaded = photosHaveSrc || false;
-    if (areAllPhotosLoaded === true) {
-      photosLoaderBtn.removeEventListener('click', loadPhotos());
-    }
+    photosHaveSrc === liBelowFoldCount && photosLoaderBtn.removeEventListener('click', loadPhotos);
   };
 
   photosLoaderBtn.addEventListener('click', loadPhotos);
